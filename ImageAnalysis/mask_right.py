e70122edc92cde32cd9ee15a28a31f7afdb4ed3a
kimflip <k.kimusan@gmail.com>
2016-12-07 00:45:20 +0900

    add_image

--- ImageAnalysis/mask_right.py
+++ ImageAnalysis/mask_right.py
@@ -3,10 +3,10 @@ import numpy as np
 
 if __name__=='__main__':
     #画像の読み込み
-    img_src = cv2.imread("./image/DSC00248.JPG",1)
+    img_src = cv2.imread("./item/DSC00248.JPG",1)
 
     #マスクの読み込み
-    img_mask = cv2.imread("./image/mask_right.png",0)
+    img_mask = cv2.imread("./item/mask_right.png",0)
     #マスクの適応
     img_masked = cv2.bitwise_and(img_src, img_src, mask=img_mask)
 
