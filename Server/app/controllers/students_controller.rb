8fe2cabb5151fb4cac9e2ca5084fd5d08fbbb9e0
villach <villach927@gmail.com>
2016-12-06 18:00:18 +0900

    Add menu for choose depertment and grade

--- Server/app/controllers/students_controller.rb
+++ Server/app/controllers/students_controller.rb
@@ -1,5 +1,11 @@
 class StudentsController < ApplicationController
   def index
-    @students = Student.all
+    @m = user_params["depertment"].empty? || user_params["grade"].empty? ?  "Error: Invalid Parameters" : user_params["depertment"] + "科" + user_params["grade"] + "年"
+    @students = Student.where("depertment = ? AND grade = ?", user_params["depertment"], user_params["grade"])
   end
+
+  private
+    def user_params
+      params
+    end
 end
