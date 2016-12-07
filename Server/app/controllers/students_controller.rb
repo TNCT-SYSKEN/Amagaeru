0a63087acfaf357a7111827273673c26f15e3ee3
villach <villach927@gmail.com>
2016-12-07 17:35:27 +0900

    Fix student list

--- Server/app/controllers/students_controller.rb
+++ Server/app/controllers/students_controller.rb
@@ -1,6 +1,10 @@
 class StudentsController < ApplicationController
   def index
-    @m = user_params["depertment"].empty? || user_params["grade"].empty? ?  "Error: Invalid Parameters" : user_params["depertment"] + "科" + user_params["grade"] + "年"
+    if user_params.include?("depertment") && user_params.include?("grade")
+      @m = user_params["depertment"].empty? || user_params["grade"].empty? ?  "Error: Invalid Parameters" : user_params["depertment"] + "科" + user_params["grade"] + "年"
+    else
+      @m = "Please select depertment and grade"
+    end
     @students = Student.where("depertment = ? AND grade = ?", user_params["depertment"], user_params["grade"])
   end
 
