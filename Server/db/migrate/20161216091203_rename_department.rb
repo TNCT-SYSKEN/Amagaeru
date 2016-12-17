7a235e6bb84096bca0c0120cdd6b171dab31c5c2
Seiji Ujihira <sejimhp@gmail.com>
2016-12-18 00:28:33 +0900

    fix sepeling

--- Server/db/migrate/20161216091203_rename_department.rb
+++ Server/db/migrate/20161216091203_rename_department.rb
@@ -1,6 +1,6 @@
 class RenameDepartment < ActiveRecord::Migration
   def change
-    rename_column :students, :department, :department
+    # rename_column :students, :department, :department
     # rename_column :seats, :department, :department
     # rename_column :attendances, :department, :department
     # rename_column :timetables, :department, :department
