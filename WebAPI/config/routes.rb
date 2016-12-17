a33098df29cccb121b37bf86426862e59d6c0736
Seiji Ujihira <sejimhp@gmail.com>
2016-12-17 14:24:47 +0900

    change route

--- WebAPI/config/routes.rb
+++ WebAPI/config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
+
+  namespace :api, {format: 'json'} do
+    namespace :v1 do
+      resources :products
+    end
+    namespace :v2 do
+      resources :products
+    end
+  end
+
   resources :products
+  root to: 'products#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
