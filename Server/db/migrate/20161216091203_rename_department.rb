6d2d0214476115545d25e87074938b8b997c9b63
Seiji Ujihira <sejimhp@gmail.com>
2016-12-17 22:04:15 +0900

    comment out

--- Server/db/migrate/20161216091203_rename_department.rb
+++ Server/db/migrate/20161216091203_rename_department.rb
@@ -1,9 +1,9 @@
 class RenameDepartment < ActiveRecord::Migration
   def change
-    rename_column :students, :depertment, :department
-    rename_column :seats, :depertment, :department
-    rename_column :attendances, :depertment, :department
-    rename_column :timetables, :depertment, :department
+    # rename_column :students, :depertment, :department
+    # rename_column :seats, :depertment, :department
+    # rename_column :attendances, :depertment, :department
+    # rename_column :timetables, :depertment, :department
 
   end
 end
